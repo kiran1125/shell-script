e29199eb31627f6505f6bf88905892550429117f
kiran1125 <90588261+kiran1125@users.noreply.github.com>
2021-09-19 16:06:38 +0530

    Add files via upload

---
+++ practice problems/practice_problem_4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+a=$((RANDOM%100))
+b=$((RANDOM%100))
+c=$((RANDOM%100))
+d=$((RANDOM%100))
+e=$((RANDOM%100))
+
+sum=$(($a + $b + $c + $d + $e))
+avg=$(($sum/5))
+
+echo "total=$sum"
+echo "average=$avg"
